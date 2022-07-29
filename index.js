2c8117ddb1ccefaa253c438955cfad34868889ef
block-doc-au <neilc@cns-gc.com.au>
2022-07-29 19:39:43 +1000

    NC Compleetd basic arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 24;
+const num4 = 5;
+
+let multiply;
+let random;
+let mod;
+let max;
+
+multiply = num1 * num2;
+
+
+random = Math.floor(Math.random()  * 10)+1;
+// console.log(random);
+
+mod = num3 % num4;
+//console.log(mod);
+
+
+max = (Math.max(5, 10, 15, 20));
+
